63bb46c45664907ad402169371795d726893e46d
Flumuffel <40270803+Flumuffel@users.noreply.github.com>
2021-07-21 10:45:54 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
 if(!isset($conn)) {
-  $conn = new PDO('mysql:host=88.198.85.222:3306;dbname=flumuffel_db4', 'flumuffel_db4', '${{SQL_PASSWORD}}');
+  $conn = new PDO('mysql:host=88.198.85.222:3306;dbname=flumuffel_db4', '${{secrets.SQL_USER}}', '${{secrets.SQL_PASSWORD}}');
 }
