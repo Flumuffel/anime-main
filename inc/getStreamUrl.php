baa009f6fc882e0bb2f2085817751d0f756009d3
Flumuffel <40270803+Flumuffel@users.noreply.github.com>
2021-07-22 12:42:57 +0200

    Update getStreamUrl.php

--- inc/getStreamUrl.php
+++ inc/getStreamUrl.php
@@ -19,24 +19,24 @@ error_reporting(E_ALL);
     $('<input id="link" type="hidden">').val(link).appendTo('body'); 
     
     $('.plyr__video-wrapper > #player > source')[0].setAttribute("src", link)
-    fetch(link)
-      .then(response => response.blob())
-      .then(blob => {
-        setFetching(false);
-        const blobURL = URL.createObjectURL(blob);
-	const name = "Ep<?php echo $params[4]; ?>"
-        const a = document.createElement("a");
-        a.href = blobURL;
-	alert(blobURL);
-        a.style = "display: none";
+    const download = () => {
+      fetch(link)
+        .then(response => response.blob())
+        .then(blob => {
+          setFetching(false);
+          const blobURL = URL.createObjectURL(blob);
+	  const name = "Ep<?php echo $params[4]; ?>"
+          const a = document.createElement("a");
+          a.href = blobURL;
+	  alert(blobURL);
+          a.style = "display: none";
 
-        if (name && name.length) a.download = name;
-        document.body.appendChild(a);
-        a.click();
-      })
-      .catch(() => setError(true));
-    $('#download')[0].setAttribute("href", link)
-    
+          if (name && name.length) a.download = name;
+          document.body.appendChild(a);
+          a.click();
+        })
+        .catch(() => alert("Error"));
+    }
     $('#deleteStreamGet').remove();
     $('.middle > button')[0].click();
     $('#player')[0].load();
