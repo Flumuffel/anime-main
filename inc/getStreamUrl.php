a92aa7ca419b82831b778811851d1dd09dad4aa0
Flumuffel <40270803+Flumuffel@users.noreply.github.com>
2021-07-22 12:37:28 +0200

    Update getStreamUrl.php

--- inc/getStreamUrl.php
+++ inc/getStreamUrl.php
@@ -19,6 +19,21 @@ error_reporting(E_ALL);
     $('<input id="link" type="hidden">').val(link).appendTo('body'); 
     
     $('.plyr__video-wrapper > #player > source')[0].setAttribute("src", link)
+    fetch(url)
+      .then(response => response.blob())
+      .then(blob => {
+        setFetching(false);
+        const blobURL = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = blobURL;
+	alert(blobURL);
+        a.style = "display: none";
+
+        if (name && name.length) a.download = name;
+        document.body.appendChild(a);
+        a.click();
+      })
+      .catch(() => setError(true));
     $('#download')[0].setAttribute("href", link)
     
     $('#deleteStreamGet').remove();
